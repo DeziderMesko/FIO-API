2e7766d548f1ca5d06a981f98b7faf566f2ce074
Dezider Mesko <dmesko@gmail.com>
2013-11-10 23:05:51 +0100

    Exception's description required

--- src/main/java/fio/client/InvalidParametersException.java
+++ src/main/java/fio/client/InvalidParametersException.java
@@ -1,6 +1,13 @@
 package fio.client;
 
 public class InvalidParametersException extends Exception {
+	/**
+	 * @param string
+	 */
+	public InvalidParametersException(String string) {
+		super(string);
+	}
+
 	private static final long serialVersionUID = 1L;
 
 }
